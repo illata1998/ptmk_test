564ab2debd797457c62f7fb179f26408f1f696ed
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-22 11:57:17 +0300

    Add mode_2.py

---
+++ ptmk_test/mode_2.py
@@ -0,0 +1,23 @@
+from ptmk_test.models import Employee
+import datetime
+
+
+def create_and_save_employee(
+        database,
+        full_name: str,
+        sex: str,
+        date_of_birth: str) -> None:
+    """
+    Creates an Employee object from passed parameters.
+    Inserts the employee data to the database.
+    """
+    employee = Employee(
+        full_name=full_name,
+        date_of_birth=datetime.datetime.strptime(
+            date_of_birth, "%Y-%m-%d"
+        ).date(),
+        sex=sex
+    )
+    database.save_employee(employee)
+    print(f'Employee {full_name} was inserted into '
+          f'the database successfully.')
