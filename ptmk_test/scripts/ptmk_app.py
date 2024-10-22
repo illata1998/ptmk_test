4df8cd93bd7cab9ebb9ef6f37d0e307a215efa35
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-22 11:57:46 +0300

    Add ptmk_app.py

---
+++ ptmk_test/scripts/ptmk_app.py
@@ -0,0 +1,40 @@
+#!/user/bin/env python3
+
+
+from ptmk_test.cli import parse_arguments
+from ptmk_test.db import EmployeeDB
+from ptmk_test.mode_1 import create_employees_table
+from ptmk_test.mode_2 import create_and_save_employee
+from ptmk_test.mode_3 import find_and_show_unique_employees
+from ptmk_test.mode_4 import fill_employees_table
+from ptmk_test.mode_5 import select_f_male_employees
+
+
+def main():
+    """
+    Manages the employees database according to
+    provided parameters parsed from the command line.
+    """
+    args = parse_arguments()
+    database = EmployeeDB(dbname='ptmk_test_db', user='postgres', password='5456',
+                 host='localhost', port='5432')
+    match args.mode:
+        case '1':
+            create_employees_table(database=database)
+        case '2':
+            create_and_save_employee(
+                database=database,
+                full_name=args.full_name,
+                sex=args.sex,
+                date_of_birth=args.date_of_birth
+            )
+        case '3':
+            find_and_show_unique_employees(database=database)
+        case '4':
+            fill_employees_table(database)
+        case '5':
+            select_f_male_employees(database=database)
+
+
+if __name__ == '__main__':
+    main()
