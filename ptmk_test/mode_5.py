036026aa03061f02655f49837e8db6502d2fbcdd
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-22 11:57:29 +0300

    Add mode_5.py

---
+++ ptmk_test/mode_5.py
@@ -0,0 +1,17 @@
+import time
+from tabulate import tabulate
+
+
+def select_f_male_employees(database):
+    """
+    Fetches male employees whose names start with the letter 'F'
+    from the 'employees' table. Measures the execution time of
+    this operation. Prints out the found employees in the form
+    of tabular data.
+    """
+    start_time = time.time()
+    employees = database.select_employees(first_letter='F', sex='Male')
+    run_time = "--- %s seconds ---" % (time.time() - start_time)
+    headers = ["Full Name", "Date of Birth", "Sex", "Age"]
+    print(tabulate(employees, headers=headers, tablefmt="fancy_grid"))
+    print(run_time)
