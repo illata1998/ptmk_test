5956a2178c4ed4efe045f832f129afd1b695a02c
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-23 16:04:21 +0300

    Update cli.py

--- ptmk_test/cli.py
+++ ptmk_test/cli.py
@@ -23,21 +23,25 @@ def parse_arguments() -> argparse.Namespace:
 
     # Operating mode of the application:
     parser.add_argument('mode', choices=['1', '2', '3', '4', '5'],
-                        metavar='MODE', help="Set the app's operating mode")
+                        metavar='MODE', help="Set the app's operating mode",
+                        type=str)
 
     # Full Name argument
     parser.add_argument('full_name', nargs='?',
                         metavar='FULL NAME',
-                        help="Employee's full name (required for mode 2)")
+                        help="Employee's full name (required for mode 2)",
+                        type=str)
 
     # Date of Birth argument
     parser.add_argument('date_of_birth', nargs='?',
                         metavar='DATE OF BIRTH',
-                        help="Employee's date of birth (required for mode 2)")
+                        help="Employee's date of birth (required for mode 2)",
+                        type=str)
 
     # Sex argument
     parser.add_argument('sex', nargs='?',
                         metavar='SEX',
-                        help="Employee's sex (required for mode 2)")
+                        help="Employee's sex (required for mode 2)",
+                        type=str)
 
     return parser.parse_args()
